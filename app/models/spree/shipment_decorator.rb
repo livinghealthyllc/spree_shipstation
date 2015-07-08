ea3f86c99c8a907a76280377c9f518f21ccf7071
Marc Leglise <mleglise@gmail.com>
2015-07-07 19:34:24 -0700

    Moved `send_shipped_email` to new ShipmentHandler decorator.

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -5,8 +5,4 @@ Spree::Shipment.class_eval do
     joins(:order).where('(spree_shipments.updated_at > ? AND spree_shipments.updated_at < ?) OR (spree_orders.updated_at > ? AND spree_orders.updated_at < ?)',from, to, from, to)
   end
 
-private
-  def send_shipped_email
-    Spree::ShipmentMailer.shipped_email(self).deliver if Spree::Config.send_shipped_email
-  end
 end
