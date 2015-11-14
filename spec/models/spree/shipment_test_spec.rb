59c93f638567d193a548a99319b908e7ffaeec20
dmitry <kosenkodmitryv@gmail.com>
2015-11-15 00:31:08 +0300

    shipment_test_spec almost the same as the shipment_spec but without the mail tests. Strange behavior of the updated_at in the create_order() method of the factory girl.

--- spec/models/spree/shipment_test_spec.rb
+++ spec/models/spree/shipment_test_spec.rb
@@ -1,113 +1,107 @@
 require 'spec_helper'
-# require 'factory_girl_rails'
 
 describe Spree::Shipment do
   context "between" do
     before do
       @active = []
-      @users = []
-      #
-      # user = FactoryGirl.create(:user, :head_buster)
-      # @users << user
 
-      create_shipment(updated_at: 2.day.ago, order: create(:order, updated_at: 2.day.ago))
-      create_shipment(updated_at: 2.day.from_now, order: create(:order, updated_at: 2.day.from_now))
-    
+      @dt1 = dt1 = 2.days.ago
+      @dt2 = dt2 = 2.day.from_now
+
+      # order = FactoryGirl.create(:order, updated_at: dt1)
+      # @shipment1 = FactoryGirl.create(:shipment, updated_at: dt1, order: order)
+
+      order = create_order(updated_at: dt1)
+      # order.updated_at = dt1
+      # order.save!
+      @shipment1 = create_shipment(updated_at: dt1, order: order)
+
+
+      # create_shipment(updated_at: dt1, order: create_order(updated_at: dt1))
+      # create_shipment(updated_at: dt1, order: create(:order, updated_at: dt1))
+      # @shipment1.updated_at = dt1
+      # @shipment1.order.updated_at = dt1
+      # @shipment1.save!
+
+      # FactoryGirl.create(:shipment, order: FactoryGirl.create(:order).update_attributes(updated_at: dt1, created_at: dt1)).update_attributes(updated_at: dt1, created_at: dt1)
+      create_shipment(updated_at: dt2, order: create_order(updated_at: dt2))
+      # FactoryGirl.create(:shipment, order: FactoryGirl.create(:order).update_attributes(updated_at: dt2, created_at: dt2)).update_attributes(updated_at: dt2, created_at: dt2)
       # Old shipment thats order was recently updated..
-      @active << create_shipment(updated_at: 1.week.ago, order: create(:order, updated_at: Time.now))
-      # New shipments
-      @active << create_shipment(updated_at: Time.now)
-      @active << create_shipment(updated_at: Time.now)
-      @orders = []
-      @orders << create(:order, updated_at: Time.now)
 
-      # ["QM","CDC","SI","QS"].each do |n|
-      FactoryGirl.create(:grau, nome: "QM")
+      @active << create_shipment(updated_at: 1.week.ago, order: create_order(updated_at: Time.now))
+      # @active << FactoryGirl.create(:shipment, updated_at: 1.week.ago, created_at: 1.week.ago, order: FactoryGirl.create(:order, updated_at: Time.now, created_at: Time.now))
+      # New shipments
+      # @active << FactoryGirl.create(:shipment, updated_at: Time.now, created_at: Time.now)
+      # @active << FactoryGirl.create(:shipment, updated_at: Time.now, created_at: Time.now)
+      @active << create_shipment(updated_at: Time.now, order: create_order(updated_at: Time.now))
+      @active << create_shipment(updated_at: Time.now, order: create_order(updated_at: Time.now))
+      # without_timestamping_of Spree::Shipment, Spree::Order do
       # end
     end
 
-    # specify { expect(@users.count).to eq(1) }
+    subject(:shipment1) { Spree::Shipment.first }
+    specify { expect(shipment1.updated_at).to eq(@dt1) }
+    it "check order date_time" do
+      expect(shipment1.order.updated_at).to eq(@dt1)
+    end
 
+    subject(:order1) { Spree::Order.first }
+    specify { expect(order1.updated_at).to eq(@dt1) }
 
+    # shipment1 = Spree::Shipment.first
+    # it "check shipment date_time" do
+    #   expect(shipment1.updated_at).to eq(@dt1)
+    # end
 
     # Get new shipments
-    from = Time.now - 1.minute
-    to = Time.now + 1.minute
+    from = Time.now - 1.hour
+    to = Time.now + 1.hour
 
-    orders = Spree::Order.where(updated_at: from..to)
+    subject(:orders) { Spree::Order.where(updated_at: from..to) }
     specify { expect(orders.size).to eq(3) }
-    specify { expect(@orders.size).to eq(1) }
 
-    shipments = Spree::Shipment.between(from, to)
+    # subject(:shipments) { Spree::Shipment.where(updated_at: from..to) }
+    subject(:shipments) { Spree::Shipment.between(from, to) }
     # shipments = Spree::Shipment.joins(:order).where(updated_at: from..to, spree_orders: { updated_at: from..to } )
     specify { expect(shipments.size).to eq(@active.size) } # check the searching of the active shipments
 
-    all_shipments = Spree::Shipment.all
+    subject(:all_shipments) { Spree::Shipment.all }
     specify { expect(all_shipments.size).to eq(5) } # check the total shipments
+
+    subject(:all_orders) { Spree::Order.all }
+    specify { expect(all_orders.size).to eq(5) } # check the total shipments
+
     specify { expect(@active.size).to eq(3) } # check size of the @active shipments
 
-
-    # subject { Spree::Shipment.between(Time.now-1.hour, Time.now + 1.hours) }
-    #
-    # # specify { should have(3).shipment }
-    # # specify { expect(subject.size).to eq(3) }
-    # specify { expect(subject.size).to include(3) }
-    #
-    # # specify { should == @active }
-    # # specify { expect(subject).to eql(@active) }
-    # specify { expect(subject.sort).to eql(@active.sort) }
-    # specify { expect(subject).to match_array(@active) }
-
   end
 
-  context "exportable" do
-    let!(:pending) { create_shipment(state: 'pending') }
-    let!(:ready)   { create_shipment(state: 'ready')   }
-    let!(:shipped) { create_shipment(state: 'shipped') }
-
-    subject { Spree::Shipment.exportable }
-
-    # specify { should have(2).shipments }
-    specify { expect(subject.count).to eq(2) }
-
-    specify { should include(ready)}
-    specify { should include(shipped)}
-
-    specify { should_not include(pending)}
-  end
-
-  context "shipped_email" do
-    let(:shipment) { create_shipment(state: 'ready') }
-
-    context "enabled" do
-      it "sends email" do
-        Spree::Config.send_shipped_email = true
-        mail_message = mock "Mail::Message"
-        Spree::ShipmentMailer.should_receive(:shipped_email).with(shipment).and_return mail_message
-        mail_message.should_receive :deliver
-        shipment.ship!
+  def without_timestamping_of(*klasses)
+    if block_given?
+      klasses.delete_if { |klass| !klass.record_timestamps }
+      klasses.each { |klass| klass.record_timestamps = false }
+      begin
+        yield
+      ensure
+        klasses.each { |klass| klass.record_timestamps = true }
       end
     end
+  end
 
-    context "disabled" do
-      it "doesnt send email" do
-        Spree::Config.send_shipped_email = false
-        Spree::ShipmentMailer.should_not_receive(:shipped_email)
-        shipment.ship!
-      end
+  def create_order(options={})
+    FactoryGirl.create(:order, options).tap do |order|
+      order.update_column(:updated_at, options[:updated_at]) if options[:updated_at]
+      # sleep(1)
+      # order.update_attributes(options)# if options[:updated_at]
     end
   end
 
   def create_shipment(options={})
-    # Spree::Shipment.create(updated_at: Time.now)
     FactoryGirl.create(:shipment, options).tap do |shipment|
       shipment.update_column(:state, options[:state]) if options[:state]
       shipment.update_column(:updated_at, options[:updated_at]) if options[:updated_at]
-      # shipment.update_column(:order, options[:order]) if options[:order]
-      # if options[:order]
-      #     shipment.order = create(:order, updated_at:Time.now)
-      #     shipment.save!
-      # end
+      # sleep(1)
+      # shipment.update_attributes(options)
+      # shipment.update_attributes(options)
     end
   end
 
